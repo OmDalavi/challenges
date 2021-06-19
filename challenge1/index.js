cbc12160c6236ac8194a15887ad6e4c343a1d63c
Om Dalavi <omdalvi184@gmail.com>
2021-06-19 17:47:55 +0530

    UI

--- challenge1/index.js
+++ challenge1/index.js
@@ -1,8 +1,9 @@
 
-$(".share").on("click",function(){
+$(".share").on("mouseover",function(){
 	$(".social").slideToggle(300);
 	
 });
-
-
+$(".share").on("mouseleave",function(){
+	$(".social").slideToggle(300);
+});
 
