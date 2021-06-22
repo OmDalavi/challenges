9b0d2e0c09d1acdb2da30ec88a48e88915a8e344
Om Dalavi <omdalvi184@gmail.com>
2021-06-22 23:45:33 +0530

    UI

--- challenge1/index.js
+++ challenge1/index.js
@@ -1,9 +1,13 @@
 
 $(".share").on("mouseover",function(){
-	$(".social").slideToggle(300);
+	// $(".social").slideToggle(300);
+	$(".social").slideDown(300);
+	setTimeout(function(){
+		$(".social").slideUp(300);
+	},5000);
 	
 });
-$(".share").on("mouseleave",function(){
-	$(".social").slideToggle(300);
-});
+// $(".share").on("mouseleave",function(){
+// 	$(".social").slideToggle(300);
+// });
 
